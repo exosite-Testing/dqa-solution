e80834c130f2559575992eadb41f21ba0e13c166
Nina Zhang <ninazhang@exosite.com>
2016-08-15 13:13:59 +0800

    Add Keystore set route (Issue: DQA-436)

--- sample_api.lua
+++ sample_api.lua
@@ -1,4 +1,4 @@
---#ENDPOINT POST /timeseries/query/
+--#ENDPOINT POST /timeseries/query
 local res
 local q = request.body.sql
 res = Timeseries.query({q = q})
@@ -16,3 +16,29 @@ local res = {}
 local q = request.parameters.sql
 res = Timeseries.command({q = q})
 return res
+
+--#ENDPOINT GET /keystore/list
+return Keystore.list()
+
+--#ENDPOINT GET /keystore/clear
+return Keystore.clear()
+
+--#ENDPOINT POST /keystore/set
+local res = Keystore.set({key=request.body.key, value=request.body.value})
+return res
+
+--#ENDPOINT GET /keystore/{key}
+res = Keystore.get({key=request.parameters.key})
+return res
+
+--#ENDPOINT DELETE /keystore/{key}
+res = Keystore.delete({key=request.parameters.key})
+return res
+
+--#ENDPOINT POST /keystore/command
+local command = request.body
+res = Keystore.command(command)
+return res
+
+--#ENDPOINT GET /customer
+return 'There is Murano, over'
